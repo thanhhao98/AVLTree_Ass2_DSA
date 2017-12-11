2da127b4db5989f5766bffcca6c85cfafcc93af7
thanhhao <hao.phanthanh98@gmail.com>
2017-12-11 23:50:09 +0700

    remove AVLTree successfully

--- main.cpp
+++ main.cpp
@@ -67,14 +67,30 @@ void AVLTree<T>::roteRL(AVLNode<T>* &pR){
 
 template <class T>
 bool AVLTree<T>::balanceLeft(AVLNode<T>* &pR){
-    pR->status=pR->_pRight->status=0;
-    return true;
+    if(pR->status==-1){
+        pR->status=pR->_pRight->status=0;
+        return true;
+    } else if(pR->status==0){
+        pR->status=1;
+        pR->_pRight->status = -1;
+        return false;
+    } else {
+        return false;
+    }
 }
 
 template <class T>
 bool AVLTree<T>::balanceRight(AVLNode<T>* &pR){
-    pR->status=pR->_pLeft->status=0;
-    return true;
+    if(pR->status==1){
+        pR->status=pR->_pLeft->status=0;
+        return true;
+    } else if(pR->status==0){
+        pR->status=-1;
+        pR->_pLeft->status=1;
+        return false;
+    } else {
+        return false;
+    }
 }
 
 template <class T>
@@ -95,23 +111,20 @@ bool AVLTree<T>::balanceLR(AVLNode<T>* &pR){
 template <class T>
 bool AVLTree<T>::balanceRL(AVLNode<T>* &pR){
     if(pR->status==0){
-        pR->_pLeft->status=0;
-        pR->_pRight->status=0;
+        pR->_pLeft->status=pR->_pRight->status=0;
     } else if(pR->status==-1){
-        pR->status=0;
-        pR->_pLeft->status=0;
+        pR->status = pR->_pLeft->status=0;
         pR->_pRight->status=1;
     } else {
-        pR->status=0;
+        pR->status = pR->_pRight->status=0;
         pR->_pLeft->status=-1;
-        pR->_pRight->status=0;
     }
     return true;
 }
 
 template <class T>
 bool AVLTree<T>::insert(AVLNode<T>* &pR, T a){
-    if(pR==NULL){
+    if(!pR){
         AVLNode<T> *newNode = new AVLNode<T>(a);
         pR = newNode;
         return true;
@@ -128,12 +141,10 @@ bool AVLTree<T>::insert(AVLNode<T>* &pR, T a){
                     return false;
                 } else if(pR->_pLeft->status == -1){
                     roteRight(pR);
-                    balanceLeft(pR);
-                    return false;
+                    return !balanceLeft(pR);
                 } else {
                     roteLR(pR);
-                    balanceLR(pR);
-                    return false;
+                    return !balanceLR(pR);
                 }
             }
         } else {
@@ -152,12 +163,10 @@ bool AVLTree<T>::insert(AVLNode<T>* &pR, T a){
                     return false;
                 } else if(pR->_pRight->status==1){
                     roteLeft(pR);
-                    balanceRight(pR);
-                    return false;
+                    return !balanceRight(pR);
                 } else {
                     roteRL(pR);
-                    balanceRL(pR);
-                    return false;
+                    return !balanceRL(pR);
                 }
             }
         } else {
@@ -168,34 +177,89 @@ bool AVLTree<T>::insert(AVLNode<T>* &pR, T a){
     }
 }
 
-// RR false
+template <class T>
+bool AVLTree<T>::remove(AVLNode<T>* &pR, T a){
+    if(!pR){
+        return false;
+    } else if(pR->data>a){
+        if(this->remove(pR->_pLeft,a)){
+            if(pR->status==0){
+                pR->status=1;
+                return false;
+            } else if(pR->status==-1){
+                pR->status=0;
+                return true;
+            } else {
+                if(pR->_pRight->status==-1){
+                    roteRL(pR);
+                    return balanceRL(pR);
+                } else {
+                    roteLeft(pR);
+                    return balanceRight(pR);
+                }
+            }
+        } else {
+            return false;
+        }
+    } else if(pR->data<a) {
+        if(this->remove(pR->_pRight,a)){
+            if(pR->status==0){
+                pR->status=-1;
+                return false;
+            } else if(pR->status==1){
+                pR->status=0;
+                return true;
+            } else {
+                if(pR->_pLeft->status==1){
+                    roteLR(pR);
+                    return balanceLR(pR);
+                } else {
+                    roteRight(pR);
+                    return balanceLeft(pR);
+                }
+            }
+        } else {
+            return false;
+        }
+    } else if(pR->data==a) {
+        if(!pR->_pLeft&&!pR->_pRight){
+            pR=NULL;
+        } else if(!pR->_pLeft){
+            pR=pR->_pRight;
+        } else if(!pR->_pRight){
+            pR=pR->_pLeft;
+        } else {
+            AVLNode<T> *temp = pR->_pRight;
+            while(temp->_pLeft){
+                temp=temp->_pLeft;
+            }
+            temp->_pLeft = pR->_pLeft;
+            pR->data = temp->data;
+            if(this->remove(pR->_pRight,pR->data)){
+                if(pR->status==0){
+                    pR->status=-1;
+                    return false;
+                } else if(pR->status==1){
+                    pR->status=0;
+                    return true;
+                } else {
+                    if(pR->_pLeft->status==1){
+                        roteLR(pR);
+                        return balanceLR(pR);
+                    } else {
+                        roteRight(pR);
+                        return balanceLeft(pR);
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+}
 
 
-int main(){
-    AVLTree<int> *root = new AVLTree<int>();
-    root->insert(50);
-    root->insert(30);
-    root->insert(80);
-    root->insert(100);   
-    root->insert(60);  
-    root->insert(70);
-    cout << root->getRoot()->data << endl;
-    cout << root->getRoot()->_pLeft->data << endl;
-    cout << root->getRoot()->_pRight->data << endl;
-    cout << root->getRoot()->_pRight->_pRight->data << endl;
-    cout << root->getRoot()->_pLeft->_pLeft->data << endl;
-    cout << root->getRoot()->_pRight->_pLeft->data << endl;
-
-
-    cout << "###################################################################" << endl;
-
-    cout << root->getRoot()->status << endl;
-    cout << root->getRoot()->_pLeft->status << endl;
-    cout << root->getRoot()->_pRight->status << endl;
-    cout << root->getRoot()->_pRight->_pRight->status << endl;
-    cout << root->getRoot()->_pLeft->_pLeft->status << endl;
-    cout << root->getRoot()->_pRight->_pLeft->status << endl;
-    
+int main(){    
 
 
 
